1617992ea10fc5a118fb759de8e9c60aa5ce8fe4
mokelgit <maym2018@gmail.com>
2023-04-04 10:46:45 -0400

    Fixed/improved some styling to better match layout

--- app/chains/[chain]/page.tsx
+++ app/chains/[chain]/page.tsx
@@ -1,9 +1,7 @@
 "use client";
 import Heading from "@/components/layout/Heading";
 import Subheading from "@/components/layout/Subheading";
-import { useMetricsData } from "@/context/MetricsProvider";
 import { useEffect, useMemo, useState } from "react";
-import { MasterResponse } from "@/types/api/MasterResponse";
 
 const Chain = ({ params }: { params: any }) => {
   // const params = useSearchParams();
@@ -19,9 +17,9 @@ const Chain = ({ params }: { params: any }) => {
         <div className="flex gap-x-6 items-center">
           <Heading className="">Chain Ex</Heading> 
         </div>
-        <div className="flex gap-x-10">
-          <button className="">Block Explorer</button>
-          <button className="">Website</button>
+        <div className="flex gap-x-10 h-10">
+          <button className="font-semibold text-white dark:bg-gray-900 bg-blue-600 px-10 py-0 rounded-full w-168px">Block Explorer</button>
+          <button className="font-semibold text-white dark:bg-gray-900 bg-blue-600 px-10 rounded-full w-120px">Website</button>
         </div>
       </div>
 
@@ -44,16 +42,16 @@ const Chain = ({ params }: { params: any }) => {
 
       {/*Metric Title Grid*/}
       <div className="w-3/4 grid grid-cols-2 gap-x-6 gap-y-8 pt-8 px-14 mx-auto justify-evenly justify-center content-evenly items-center">
-          <div className="bg-white rounded-md h-60">
+          <div className="dark:bg-gray-900 bg-blue-600 rounded-xl h-72">
               <h1>{metricTitle}</h1>
           </div>
-          <div className="bg-white rounded-md h-60">
+          <div className="dark:bg-gray-900 bg-blue-600 rounded-xl h-72">
               <p>test</p>
           </div>
-          <div className="bg-white rounded-md h-60">
+          <div className="dark:bg-gray-900 bg-blue-600 rounded-xl h-72">
               <p>test</p>
           </div>
-          <div className="bg-white rounded-md h-60">
+          <div className="dark:bg-gray-900 bg-blue-600 rounded-xl h-72">
               <p>test</p>            
           </div>
           {/*Generate cards in here in future*/}
