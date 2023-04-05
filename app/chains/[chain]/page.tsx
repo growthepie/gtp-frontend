218e6517c633addbf633f5e40679fb4ea85f599b
manishiwa <manishiwa@gmail.com>
2023-04-05 12:05:45 -0400

    cleaning up - removed twind

--- app/chains/[chain]/page.tsx
+++ app/chains/[chain]/page.tsx
@@ -31,8 +31,8 @@ const Chain = ({ params }: { params: any }) => {
 
       <Subheading className="text-lg font-medium mt-10 w-2/5">
         Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
+        industry. Lorem Ipsum has been the industry&apos;s standard dummy text
+        ever since the 1500s, when an unknown printer took a galley of type and
         scrambled it to make a type specimen book.
       </Subheading>
 
--- components/layout/ComparisonChart.tsx
+++ components/layout/ComparisonChart.tsx
@@ -8,7 +8,7 @@ import { Card } from "@/components/Card";
 import { useLocalStorage, useSessionStorage } from "usehooks-ts";
 import fullScreen from "highcharts/modules/full-screen";
 import _merge from "lodash/merge";
-import { zinc, red, blue, amber, purple } from "@twind/preset-tailwind/colors";
+import { zinc, red, blue, amber, purple } from "tailwindcss/colors";
 import { ArrowTrendingUpIcon } from "@heroicons/react/24/outline";
 import { useTheme } from "next-themes";
 
