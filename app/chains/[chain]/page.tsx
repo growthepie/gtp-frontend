aac11703438f806d96db58063e96c597cc8e8bce
mokelgit <maym2018@gmail.com>
2023-04-11 14:35:23 -0400

    SlGot web data for chains page. Polygon and mMasterResponse need to be updated for v0_3 changes

--- app/chains/[chain]/page.tsx
+++ app/chains/[chain]/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 import { useEffect, useMemo, useState } from "react";
+import Link from 'next/link'
 import {
   BanknotesIcon
 } from "@heroicons/react/24/solid";
 import {
   ArrowTopRightOnSquareIcon,
-  LinkIcon
+  LinkIcon,
+  AtSymbolIcon
 } from '@heroicons/react/24/outline'
 import { dataTool } from "echarts";
 import { Time } from "highcharts";
@@ -19,37 +21,47 @@ const Chain = ({ params }: { params: any }) => {
   // const chain = params.get("chain");
   const { chain } = params;
 
-  const [pageData, setPageData] = useState(String(chain).charAt(0).toUpperCase() + String(chain).slice(1));
+  const [pageName, setPageName] = useState(String(chain).charAt(0).toUpperCase() + String(chain).slice(1));
+  const [pageChain, setPageChain] = useState(null)
   const [metricTitle, setMetricTitle] = useState("");
   const [value, setValue] = useState("");
   const [buttons, setButtons] = useState();
+  const [data, setData] = useState(null);
 
 
   const { data: master, error: masterError } = useSWR<MasterResponse>(
-    "https://d2cfnw27176mbd.cloudfront.net/v0_2/master.json"
+    "https://d2cfnw27176mbd.cloudfront.net/v0_3/master.json"
   );
 
   const { data: daa, error: daaError } = useSWR<DAAMetricsResponse>(
-    "https://d2cfnw27176mbd.cloudfront.net/v0_2/metrics/daa.json"
+    "https://d2cfnw27176mbd.cloudfront.net/v0_3/metrics/daa.json"
   );
 
   /*Create some kind of map for metric cards*/
   
 
-  if(master){
-    console.log(AllChains.filter(
-        (chain) => Object.keys(master)
-    )[0]);
-  }
-
   const chainData = useMemo(() => {
     if (!master) return [];
+
+    for (let chainName in master.chains) {
+      if (chainName === chain) {
+        return master.chains[chainName];
+      }
+    }
     
     
-  }, [master]) 
+  }, [master, chain]) 
+
+  if(!master){
+    return(
+    <div className="flex justify-center items-center h-screen">
+      Loading...
+    </div>
+    );
+  }
 
   return (
-    <div className="mx-auto pt-10 w-44[rem] sm:w-[88rem]">
+    <div className="mx-auto pt-10 w-44[rem] lg:w-[88rem]">
         {/*Header */}
       <div className="ml-12 mr-14">
         <div className="flex justify-between items-center">
@@ -59,19 +71,28 @@ const Chain = ({ params }: { params: any }) => {
               {/*Uppercase first letter */}
             </div>
             <div className="flex gap-x-10 h-10">
-              <div className="flex justify-between text-white dark:bg-[#2A3433] dark:text-[#CDD8D3] bg-blue-600 w-44 py-0 rounded-full w-168px pl-4 pr-6">
+              <Link href={(chainData.block_explorer)} className="flex justify-between text-white dark:bg-[#2A3433] dark:text-[#CDD8D3] bg-blue-600 w-44 py-0 rounded-full w-168px pl-4 pr-6">
                 <LinkIcon className="h-4 w-4 self-center" />
                 <p className="self-center font-semibold">Block Explorer</p>
-              </div>
-              <div className="flex justify-between text-white dark:bg-[#2A3433]  dark:text-[#CDD8D3] bg-blue-600 w-32 py-0 rounded-full w-168px pl-4 pr-6">
+              </Link>
+              
+              <Link href={(chainData.twitter)} className="flex justify-between text-white dark:bg-[#2A3433]  dark:text-[#CDD8D3] bg-blue-600 w-40 py-0 rounded-full w-168px pl-4 pr-6">
+                <AtSymbolIcon className="h-4 w-4 self-center" />
+                <p className="self-center font-semibold">{chainData.twitter.split( "https://twitter.com/")}</p>
+              </Link>
+              
+              <Link href={(chainData.website)} className="flex justify-between text-white dark:bg-[#2A3433]  dark:text-[#CDD8D3] bg-blue-600 w-32 py-0 rounded-full w-168px pl-4 pr-6">
                 <ArrowTopRightOnSquareIcon className="h-4 w-4 self-center" />
                 <p className="self-center font-semibold">Website</p>
-              </div>
+              </Link>
+
+
+              
             </div>
         </div>
       
-      <h1 className="text-lg text-gray-500 pt-4 font-[600] dark:text-[#CDD8D3] w-2/5">
-        Lorem Ipsum about {String(chain).charAt(0).toUpperCase() + String(chain).slice(1)}
+      <h1 className="text-lg text-gray-500 pt-4 font-[600] pl-1 dark:text-[#CDD8D3] w-2/5">
+        Lorem Ipsum about {pageName}
       </h1>
       </div>
       {/*Time selection */}
@@ -100,7 +121,7 @@ const Chain = ({ params }: { params: any }) => {
 
       {/*Metric Title Grid*/}
       <div className="flex-col pt-8">
-        <div className="flex flex-col gap-x-6 justify-start ml-12 gap-y-8 md:flex-row md:justify-center md:ml-0 md:gap-y-0">
+        <div className="flex flex-col gap-x-6 justify-start ml-12 gap-y-8 lg:flex-row lg:justify-center lg:ml-0 lg:gap-y-0">
           <div className="dark:bg-[#2A3433] bg-blue-600 rounded-xl w-[40rem] h-[20rem]">
             <h1 className="pt-[1rem] pl-6 text-3xl font-[700] text-transparent bg-gradient-to-r bg-clip-text  from-[#9DECF9] to-[#2C5282] dark:text-[#CDD8D3]">Metric Title</h1>
             <BanknotesIcon className="relative h-[75px] w-[94px] text-blue-500 bottom-[1.7rem] left-[32rem] mr-4 dark:text-[#CDD8D3]"/>
@@ -115,7 +136,7 @@ const Chain = ({ params }: { params: any }) => {
             </div>
           </div>
         </div>
-        <div className="flex flex-col pt-8 gap-x-6 justify-start ml-12 gap-y-8 md:flex-row md:justify-center md:ml-0 md:gap-y-0">
+        <div className="flex flex-col pt-8 gap-x-6 justify-start ml-12 gap-y-8 lg:flex-row lg:justify-center lg:ml-0">
           <div className="dark:bg-[#2A3433] bg-blue-600 rounded-xl w-[40rem] h-[20rem]">
             <div className="dark:bg-[#2A3433]bg-blue-600 rounded-xl w-[40rem] h-[20rem]">
               <h1 className="pt-[1rem] pl-6 text-3xl font-[700] text-transparent bg-gradient-to-r bg-clip-text  from-[#9DECF9] to-[#2C5282] dark:text-[#CDD8D3]">Metric Title</h1>
