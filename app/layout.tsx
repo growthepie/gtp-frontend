95dd73ff90ec80a83675fe41c57787335fcf20eb
Manish Gupta <manishiwa@gmail.com>
2023-04-15 13:34:51 -0700

    new Cookie Consent system using Google Tag manager for Analytics consent

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,20 @@
 "use client";
-import { useLocalStorage, useMediaQuery } from "usehooks-ts";
-import DarkModeSwitch from "@/components/layout/DarkModeSwitch";
-import { Providers } from "./providers";
 import "./globals.css";
+import { useEffect, useState } from "react";
 import Image from "next/image";
-
+import Script from "next/script";
+import Link from "next/link";
 import { motion } from "framer-motion";
+import { useLocalStorage, useMediaQuery } from "usehooks-ts";
+import { Providers } from "./providers";
+import { getCookie } from "cookies-next";
+import CookieConsent from "@/components/layout/CookieConsent";
+import DarkModeSwitch from "@/components/layout/DarkModeSwitch";
 import EthUsdSwitch from "@/components/layout/EthUsdSwitch";
 import Sidebar from "@/components/layout/Sidebar";
-import { Bars3Icon } from "@heroicons/react/24/solid";
-import { useEffect, useState } from "react";
 import Loader from "@/components/Loader";
-import Link from "next/link";
 import { Icon } from "@iconify/react";
 import { Raleway } from "@next/font/google";
-import CookieConsent from "react-cookie-consent";
 
 // If loading a variable font, you don't need to specify the font weight
 const raleway = Raleway({
@@ -28,6 +28,8 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const consent = getCookie("growthepieCookieConsent");
+
   const isLargeScreen = useMediaQuery("(min-width: 768px)");
 
   const [startSidebarOpen, setStartSidebarOpen] = useState(false);
@@ -44,76 +46,62 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
+      <Script
+        id="gtag"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+
+            gtag('consent', 'default', {
+              'ad_storage': 'denied',
+              'analytics_storage': 'denied'
+            });
+
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-WK73L5M');`,
+        }}
+      />
+      {consent === true && (
+        <Script
+          id="consupd"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            gtag('consent', 'update', {
+              'ad_storage': 'granted',
+              'analytics_storage': 'granted'
+            });
+          `,
+          }}
+        />
+      )}
       <body
         className={`bg-forest-50/30 bg-gradient-to-r dark:from-forest-800 dark:via-forest-900 dark:to-forest-900 text-forest-900 ${raleway.variable} font-raleway`}
       >
-        <div className="absolute top-0 left-0 -z-10 flex w-full justify-center overflow-clip">
-          <div className="h-[500px] xs:h-[650px] md:h-[700px] xl:h-[800px] 2xl:h-[900px] 3xl:h-[1200px]" />
-          <div className=" absolute -top-[20px] h-full w-[150vw] select-none xs:-top-[20px] sm:-top-[60px] md:-top-[100px] lg:-top-[120px] xl:-top-[120px] 2xl:-top-[150px]">
-            <span className=" box-border block overflow-hidden absolute top-0 left-0 right-0 bottom-0">
-              <Image
-                src="/bg-glow.png"
-                alt="Forest"
-                className="opacity-30 box-border block overflow-hidden absolute top-0 left-0 xs:left-[10vw] md:left-[45vw] lg:left-[55vw] xl:left-[65vw] bottom-0 m-auto min-w-full max-w-[150vw] w-[150vw] min-h-full max-h-[150vh] object-cover sm:object-scale-down mix-blend-soft-light"
-                fill={true}
-                quality={100}
-                priority={true}
-              />
-            </span>
-          </div>
-        </div>
-
-        {/* large centered loading animation */}
-        {/* <div
-            id="main-loader"
-            className="fixed inset-0 flex items-center justify-center"
-          >
-            <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900 "></div>
-          </div> */}
-        {/* <div className="min-h-screen max-full mx-auto p-4 sm:p-6 lg:p-8"> */}
         <Providers>
-          <CookieConsent
-            disableStyles={true}
-            contentStyle={{}}
-            enableDeclineButton
-            flipButtons
-            location="bottom"
-            cookieName="GrowThePieCookieConsent"
-            // style={{ background: "black" }}
-            buttonText="Allow All"
-            declineButtonText="Decline"
-            buttonClasses="bg-white text-forest-900 p-2 lg:p-4 rounded-full text-center whitespace-nowrap w-full lg:w-auto"
-            declineButtonClasses="bg-white text-forest-900 p-2 lg:p-4 rounded-full text-center whitespace-nowrap w-full lg:w-auto"
-            containerClasses="flex flex-col lg:flex-row items-center justify-between space-y-4 lg:space-y-0 lg:space-x-8 bg-forest-50 text-forest-900 border-forest-200 border-[1px] bottom-0 mb-10 fixed z-50 w-5/6 mx-auto rounded-3xl lg:rounded-full px-10 py-8 left-[50%] transform -translate-x-1/2 shadow-2xl"
-            contentClasses="flex items-center justify-center space-x-4"
-            buttonWrapperClasses="flex items-center justify-center w-full lg:w-auto space-y-4 lg:space-y-0 lg:space-x-8 flex-col lg:flex-row"
-            expires={450}
-          >
-            <div>
-              <Icon
-                icon="feather:shield"
-                className="w-24 h-24 lg:w-16 lg:h-16"
-              />
+          {/* <div className="flex flex-col min-h-screen max-h-screen h-screen"> */}
+          {/* <background gradient> */}
+          <div className="absolute top-0 left-0 -z-10 flex w-full justify-center overflow-clip">
+            <div className="h-[500px] xs:h-[650px] md:h-[700px] xl:h-[800px] 2xl:h-[900px] 3xl:h-[1200px]" />
+            <div className=" absolute -top-[20px] h-full w-[150vw] select-none xs:-top-[20px] sm:-top-[60px] md:-top-[100px] lg:-top-[120px] xl:-top-[120px] 2xl:-top-[150px]">
+              <span className=" box-border block overflow-hidden absolute top-0 left-0 right-0 bottom-0">
+                <Image
+                  src="/bg-glow.png"
+                  alt="Forest"
+                  className="opacity-30 box-border block overflow-hidden absolute top-0 left-0 xs:left-[10vw] md:left-[45vw] lg:left-[55vw] xl:left-[65vw] bottom-0 m-auto min-w-full max-w-[150vw] w-[150vw] min-h-full max-h-[150vh] object-cover sm:object-scale-down mix-blend-soft-light"
+                  fill={true}
+                  quality={100}
+                  priority={true}
+                />
+              </span>
             </div>
-            <div className="flex items-center space-x-8">
-              <div className="">
-                Our website uses cookies to analyze how the site is used and to
-                ensure your experience is consistent between visits.
-                <br />
-                Find our{" "}
-                <Link href="/privacy-policy" className="underline relative">
-                  Privacy Policy here.
-                </Link>
-                {/* <Link href="/privacy-policy" className="underline relative">
-                  <div className="inline-flex underline absolute -bottom-0 left-4 w-40">
-                    <Icon icon="fe:link-external" className="w-5 h-5" />
-                    <div className="ml-1 text-sm">Privacy Policy</div>
-                  </div>
-                </Link> */}
-              </div>
-            </div>
-          </CookieConsent>
-          {/* <button className="bg-blue-100 w-full">s</button> */}
+          </div>
+          {/* </background gradient> */}
           <div className="flex justify-between h-fit w-full">
             <div className="pt-6 pl-4 bg-forest-100 dark:bg-forest-900 border-forest-400 mix-h-screen max-h-full hidden md:flex overflow-hidden flex-col">
               <Link href="/" className="relative h-[45px] block mb-4" passHref>
@@ -194,12 +182,14 @@ export default function RootLayout({
                   </Link>
                 </div>
               </div>
-              <main className="flex-1 overflow-y-auto max-w-[1600px] w-full mx-auto">
+              <main className="flex-1 max-w-[1600px] w-full mx-auto overflow-auto">
                 {children}
               </main>
             </div>
           </div>
           {/* <Loader /> */}
+          {/* </div> */}
+          <CookieConsent />
         </Providers>
       </body>
     </html>
---
+++ components/layout/CookieConsent.tsx
@@ -0,0 +1,94 @@
+import React, { useEffect, useState } from "react";
+import { setCookie, hasCookie } from "cookies-next";
+import { Icon } from "@iconify/react";
+import Link from "next/link";
+
+export default function CookieConsent() {
+  const [consent, setConsent] = useState(true);
+  useEffect(() => {
+    setConsent(hasCookie("growthepieCookieConsent"));
+  }, []);
+
+  const acceptCookie = () => {
+    setConsent(true);
+    setCookie("growthepieCookieConsent", "true", {
+      maxAge: 60 * 60 * 24 * 365,
+    });
+
+    gtag("consent", "update", {
+      ad_storage: "granted",
+      analytics_storage: "granted",
+    });
+
+    console.log("accepting cookies");
+  };
+
+  const closeP = () => {
+    setConsent(true);
+    console.log("closing");
+  };
+
+  const denyCookie = () => {
+    setConsent(true);
+    setCookie("growthepieCookieConsent", "false", {
+      maxAge: 60 * 60 * 24 * 365,
+    });
+    console.log("denying cookies");
+  };
+
+  if (consent === true) {
+    return null;
+  }
+
+  return (
+    <div
+      className={`flex flex-col lg:flex-row items-center justify-between px-10 py-8  space-y-4 lg:space-y-0 lg:space-x-8 bg-forest-50 text-forest-900 border-forest-200 border-[1px] bottom-0 mb-10 fixed z-50 w-5/6 mx-auto rounded-3xl lg:rounded-full left-[50%] transform -translate-x-1/2 shadow-2xl ${
+        consent ? "hidden" : ""
+      }`}
+    >
+      <div className="flex items-center justify-center space-x-10">
+        <div>
+          <Icon icon="feather:shield" className="w-24 h-24 lg:w-16 lg:h-16" />
+        </div>
+        <div className="flex items-center space-x-8">
+          <div className="">
+            Our website uses cookies to analyze how the site is used and to
+            ensure your experience is consistent between visits.
+            <br />
+            Find our{" "}
+            <Link href="/privacy-policy" className="underline relative">
+              Privacy Policy here.
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex align-middle justify-between">
+        <div className="flex items-center justify-center w-full space-x-8">
+          <button
+            onClick={() => {
+              acceptCookie();
+            }}
+            className="bg-white/70 dark:bg-black/40 text-forest-900 px-5 py-2 lg:py-3 rounded-full text-center whitespace-nowrap w-full lg:w-auto hover:bg-white"
+          >
+            Allow All
+          </button>
+          <button
+            onClick={(e) => denyCookie()}
+            className="bg-white/70 dark:bg-black/40 text-forest-900 px-5 py-2 lg:py-3 rounded-full text-center whitespace-nowrap w-full lg:w-auto hover:bg-white"
+          >
+            Decline
+          </button>
+        </div>
+      </div>
+      {/* <button
+          className="self-start"
+          onClick={(e) => {
+            closeP();
+          }}
+        >
+          <Icon icon="feather:x" className="w-8 h-8" />
+        </button> */}
+    </div>
+  );
+}
