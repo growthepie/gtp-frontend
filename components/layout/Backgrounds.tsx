22928e268732de8f146b354a9dea0a06e276a75a
manishiwa <manishiwa@gmail.com>
2023-05-26 11:27:29 -0400

    disabling bg gradients on mobile

--- components/layout/Backgrounds.tsx
+++ components/layout/Backgrounds.tsx
@@ -1,4 +1,4 @@
-import { isMobile } from "react-device-detect";
+// import { isMobile } from "react-device-detect";
 
 type BackgroundsProps = {
   isMobileMenu?: boolean;
@@ -15,41 +15,39 @@ export default function Backgrounds({ isMobileMenu }: BackgroundsProps) {
           }}
           className="fixed inset-0 z-20 mouse-events-none overflow-hidden w-full h-full bg-white dark:bg-[#1F2726]"
         ></div>
-        {!isMobile && (
+        <div
+          style={{
+            mixBlendMode: "screen",
+            opacity: 0.065,
+            pointerEvents: "none",
+          }}
+          className="fixed inset-0 z-20 mouse-events-none overflow-hidden w-full h-full  hidden md:dark:block antialiased"
+        >
           <div
             style={{
-              mixBlendMode: "screen",
-              opacity: 0.065,
-              pointerEvents: "none",
+              height: "600px",
+              width: "500px",
+              left: "100px",
+              right: "-6px",
+              top: "0px",
+              bottom: "602px",
+              background: `radial-gradient(45% 45% at 50% 50%, #FBB90D 0%, rgba(217, 217, 217, 0) 100%, rgba(251, 185, 13, 0) 100%)`,
             }}
-            className="fixed inset-0 z-20 mouse-events-none overflow-hidden w-full h-full hidden dark:block antialiased"
-          >
-            <div
-              style={{
-                height: "600px",
-                width: "500px",
-                left: "100px",
-                right: "-6px",
-                top: "0px",
-                bottom: "602px",
-                background: `radial-gradient(45% 45% at 50% 50%, #FBB90D 0%, rgba(217, 217, 217, 0) 100%, rgba(251, 185, 13, 0) 100%)`,
-              }}
-              className="absolute z-0 mouse-events-none"
-            ></div>
-            <div
-              style={{
-                height: "600px",
-                width: "700px",
-                left: "275px",
-                right: "-475px",
-                top: "0px",
-                bottom: "466px",
-                background: `radial-gradient(45% 45% at 50% 50%, #0DF6B9 0%, rgba(217, 217, 217, 0) 100%, rgba(13, 246, 185, 0) 100%)`,
-              }}
-              className="absolute z-0 mouse-events-none"
-            ></div>
-          </div>
-        )}
+            className="absolute z-0 mouse-events-none"
+          ></div>
+          <div
+            style={{
+              height: "600px",
+              width: "700px",
+              left: "275px",
+              right: "-475px",
+              top: "0px",
+              bottom: "466px",
+              background: `radial-gradient(45% 45% at 50% 50%, #0DF6B9 0%, rgba(217, 217, 217, 0) 100%, rgba(13, 246, 185, 0) 100%)`,
+            }}
+            className="absolute z-0 mouse-events-none"
+          ></div>
+        </div>
       </>
     );
 
@@ -62,41 +60,41 @@ export default function Backgrounds({ isMobileMenu }: BackgroundsProps) {
         }}
         className="absolute z-0 mouse-events-none overflow-hidden w-full h-full hidden dark:block dark:bg-forest-1000"
       ></div>
-      {!isMobile && (
+      {/* {!isMobile && ( */}
+      <div
+        style={{
+          mixBlendMode: "overlay",
+          opacity: 0.3,
+          pointerEvents: "none",
+        }}
+        className="absolute z-0 mouse-events-none overflow-hidden w-full h-full hidden md:dark:block"
+      >
         <div
           style={{
-            mixBlendMode: "overlay",
-            opacity: 0.3,
-            pointerEvents: "none",
+            height: "1215px",
+            width: "1026px",
+            left: "131px",
+            right: "-6px",
+            top: "-90px",
+            bottom: "602px",
+            background: `radial-gradient(45% 45% at 50% 50%, #FBB90D 0%, rgba(217, 217, 217, 0) 100%, rgba(251, 185, 13, 0) 100%)`,
           }}
-          className="absolute z-0 mouse-events-none overflow-hidden w-full h-full hidden dark:block"
-        >
-          <div
-            style={{
-              height: "1215px",
-              width: "1026px",
-              left: "131px",
-              right: "-6px",
-              top: "-90px",
-              bottom: "602px",
-              background: `radial-gradient(45% 45% at 50% 50%, #FBB90D 0%, rgba(217, 217, 217, 0) 100%, rgba(251, 185, 13, 0) 100%)`,
-            }}
-            className="absolute z-0 mouse-events-none"
-          ></div>
-          <div
-            style={{
-              height: "1274px",
-              width: "1405px",
-              left: "410px",
-              right: "-475px",
-              top: "-90px",
-              bottom: "466px",
-              background: `radial-gradient(45% 45% at 50% 50%, #0DF6B9 0%, rgba(217, 217, 217, 0) 100%, rgba(13, 246, 185, 0) 100%)`,
-            }}
-            className="absolute z-0 mouse-events-none"
-          ></div>
-        </div>
-      )}
+          className="absolute z-0 mouse-events-none"
+        ></div>
+        <div
+          style={{
+            height: "1274px",
+            width: "1405px",
+            left: "410px",
+            right: "-475px",
+            top: "-90px",
+            bottom: "466px",
+            background: `radial-gradient(45% 45% at 50% 50%, #0DF6B9 0%, rgba(217, 217, 217, 0) 100%, rgba(13, 246, 185, 0) 100%)`,
+          }}
+          className="absolute z-0 mouse-events-none"
+        ></div>
+      </div>
+      {/* )} */}
     </>
   );
 }
