117917dbe3737713f9907376f26e9f29e5284685
mokelgit <maym2018@gmail.com>
2023-08-19 19:02:57 -0400

    WIP: overview metrics

--- components/layout/OverviewMetrics.tsx
+++ components/layout/OverviewMetrics.tsx
@@ -168,6 +168,7 @@ export default function OverviewMetrics({
     };
   });
 
+  console.log(data.all_l2s["overview"]);
   useEffect(() => {
     // Process the data and create the contracts object
     const result: { [key: string]: ContractInfo } = {};
@@ -175,8 +176,9 @@ export default function OverviewMetrics({
     for (const category of Object.keys(data)) {
       if (data) {
         const contractsData =
-          data.all_l2s.overview[selectedTimespan].contracts.data;
-        const types = data.all_l2s.overview[selectedTimespan].contracts.types;
+          data.all_l2s["overview"].overview[selectedTimespan].contracts.data;
+        const types =
+          data.all_l2s["overview"].overview[selectedTimespan].contracts.types;
 
         for (const contract of Object.keys(contractsData)) {
           const dataArray = contractsData[contract];
