e3d201afe766abac431b9818a102f6cfe8db9284
mokelgit <maym2018@gmail.com>
2023-11-15 09:15:29 -0500

    Updated banner styles

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -22,11 +22,13 @@ export default function Banner() {
       href={
         isFirstText
           ? "https://vote.optimism.io/retropgf/3/application/0x849e164d1b8cc2b51bd3313d007ac58acb816660492336d2498615551ec75f02"
-          : "https://explorer.gitcoin.co/#/round/10/0x8de918f0163b2021839a8d84954dd7e8e151326d/0x8de918f0163b2021839a8d84954dd7e8e151326d-48"
+          : "https://explorer.gitcoin.co/#/round/424/0xd4cc0dd193c7dc1d665ae244ce12d7fab337a008/0xd4cc0dd193c7dc1d665ae244ce12d7fab337a008-70"
       }
       target="_blank"
       rel="noopener"
-      className="hidden overflow-hidden xl:flex absolute top-[50px] items-center dark:bg-[#FFE28A] text-black bg-forest-100 p-1 justify-between rounded-full text-sm font-bold mr-auto"
+      className={`hidden overflow-hidden xl:flex absolute top-[50px] items-center dark:bg-[#FFE28A]  text-black bg-forest-100 p-1 justify-between rounded-full text-sm font-bold mr-auto ${
+        isFirstText ? "dark:bg-[#FFE28A]" : "dark:bg-[#1DF7EF]"
+      }`}
     >
       <div
         className="ml-3 mr-4 flex space-x-0.5 overflow-x-visible"
@@ -43,9 +45,9 @@ export default function Banner() {
           <div className="slide-text mr-[200px]">
             We are listed for Retroactive Public Goods Funding. Spread the word!{" "}
           </div>
-          <div className="slide-text flex">
-            <div>We are part of Gitcoin Round 19!.</div>
-            <div className="animate-bounce text-sm">🎉</div>{" "}
+          <div className="slide-text flex ">
+            <div className="mr-1.5">We are part of Gitcoin Round 19!</div>
+            <div className="animate-bounce text-sm"> 🎉</div>{" "}
             <div>Help us by donating to our project.</div>
           </div>
         </div>
@@ -53,7 +55,9 @@ export default function Banner() {
 
       <Icon
         icon="feather:arrow-right-circle"
-        className="h-6 w-6 dark:bg-[#FFE28A] bg-forest-100 z-10"
+        className={`h-6 w-6  bg-forest-100 z-10 ${
+          isFirstText ? "dark:bg-[#FFE28A]" : "dark:bg-[#1DF7EF]"
+        }`}
       />
     </Link>
   );
