1aef3756ee85cb6ea3dff01d43e2805409e98ed0
mokelgit <maym2018@gmail.com>
2023-11-15 09:33:58 -0500

    Added Banner component to main

---
+++ components/Banner.tsx
@@ -0,0 +1,64 @@
+"use client";
+import Link from "next/link";
+import { useState, useEffect } from "react";
+import { Icon } from "@iconify/react";
+
+export default function Banner() {
+  const [isFirstText, setIsFirstText] = useState(true);
+
+  useEffect(() => {
+    // Toggle between texts every 8 seconds
+    const textSlideInterval = setInterval(() => {
+      setIsFirstText((prevIsFirstText) => !prevIsFirstText);
+    }, 8000);
+
+    return () => {
+      clearInterval(textSlideInterval);
+    };
+  }, []);
+
+  return (
+    <Link
+      href={
+        isFirstText
+          ? "https://vote.optimism.io/retropgf/3/application/0x849e164d1b8cc2b51bd3313d007ac58acb816660492336d2498615551ec75f02"
+          : "https://explorer.gitcoin.co/#/round/424/0xd4cc0dd193c7dc1d665ae244ce12d7fab337a008/0xd4cc0dd193c7dc1d665ae244ce12d7fab337a008-70"
+      }
+      target="_blank"
+      rel="noopener"
+      className={`hidden overflow-hidden xl:flex absolute top-[50px] items-center text-black bg-forest-100 p-1 justify-between rounded-full text-sm font-bold mr-auto ${
+        isFirstText ? "dark:bg-[#FFE28A]" : "dark:bg-[#1DF7EF]"
+      }`}
+    >
+      <div
+        className="ml-3 mr-4 flex space-x-0.5 overflow-x-visible"
+        style={{
+          width: "500px", // Adjust the width as needed
+          transition: "transform 1s ease-in-out", // Adjust the duration and easing as needed
+          transform: `translateX(${isFirstText ? "0%" : "-665px"})`,
+        }}
+      >
+        <div
+          className="w-[800px] flex space-x-0.5 z-0"
+          style={{ minWidth: 0, whiteSpace: "nowrap" }}
+        >
+          <div className="slide-text mr-[200px]">
+            We are listed for Retroactive Public Goods Funding. Spread the word!{" "}
+          </div>
+          <div className="slide-text flex ">
+            <div className="mr-1.5">We are part of Gitcoin Round 19!</div>
+            <div className="animate-bounce text-sm"> 🎉</div>{" "}
+            <div>Help us by donating to our project.</div>
+          </div>
+        </div>
+      </div>
+
+      <Icon
+        icon="feather:arrow-right-circle"
+        className={`h-6 w-6  bg-forest-100 z-10 ${
+          isFirstText ? "dark:bg-[#FFE28A]" : "dark:bg-[#1DF7EF]"
+        }`}
+      />
+    </Link>
+  );
+}
--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -7,26 +7,14 @@ import EthUsdSwitch from "./EthUsdSwitch";
 import DarkModeSwitch from "./DarkModeSwitch";
 import { useUIContext } from "@/contexts/UIContext";
 import { useSpring, animated, config } from "react-spring";
+import Banner from "@/components/Banner";
 
 export default function Header() {
   return (
     <header className="flex flex-col xl:flex-row justify-between space-x-0 xl:space-x-6 items-center max-w-[1600px] w-full mx-auto px-[20px] pt-[20px] md:px-[50px] md:pt-[50px]">
       <div className="flex justify-between items-center w-full">
         <div className="flex space-x-0 xl:space-x-6 w-full">
-          <Link
-            href="https://vote.optimism.io/retropgf/3/application/0x849e164d1b8cc2b51bd3313d007ac58acb816660492336d2498615551ec75f02"
-            target="_blank"
-            rel="noopener"
-            className="hidden xl:flex absolute top-[50px] items-center dark:bg-[#FFE28A] text-black bg-forest-100 p-1 justify-between rounded-full text-sm font-bold mr-auto"
-          >
-            <div className="ml-3 mr-4 flex space-x-0.5">
-              <div>
-                We are listed for Retroactive Public Goods Funding. Spread the
-                word!{" "}
-              </div>
-            </div>
-            <Icon icon="feather:arrow-right-circle" className="h-6 w-6" />
-          </Link>
+          <Banner />
 
           <div className="flex justify-between items-end md:hidden relative w-full">
             <Link href="/" className="">
