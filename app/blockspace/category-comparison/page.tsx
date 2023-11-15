bc72d7f65ea4edb0a2715f89d29150e69e4f97cc
mokelgit <maym2018@gmail.com>
2023-11-14 21:17:32 -0500

    Added Category Comparison QnA

--- app/blockspace/category-comparison/page.tsx
+++ app/blockspace/category-comparison/page.tsx
@@ -10,6 +10,7 @@ import { BlockspaceURLs } from "@/lib/urls";
 import useSWR from "swr";
 import { CategoryComparisonResponse } from "@/types/api/CategoryComparisonResponse";
 import EcosystemDropdown from "@/components/layout/EcosystemDropdown";
+import QuestionAnswer from "@/components/layout/QuestionAnswer";
 
 const CategoryComparison = () => {
   const {
@@ -64,6 +65,21 @@ const CategoryComparison = () => {
           setSelectedTimespan={setSelectedTimespan}
           data={usageData.data}
         />
+      )}
+      {usageData && (
+        <Container className="flex flex-col space-y-[15px] mt-[45px]">
+          <QuestionAnswer
+            className="rounded-3xl bg-forest-50 dark:bg-forest-900 px-[63px] py-[23px] flex flex-col"
+            question={`What does Category Comparison tell you?`}
+            answer={`We measure the gas fees spent and the number of transactions sent to
+            smart contracts. The smart contracts are mapped to distinct
+            categories. The chart below allows to compare the usage of these
+            categories across different chains. Each category is made up of
+            multiple subcategories. You can click on the category dropdown in
+            order to see and filter its subcategories.`}
+            startOpen
+          />
+        </Container>
       )}
     </>
   );
