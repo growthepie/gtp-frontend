770a4fa3432b3ce3610352db0a5633f1bef35ca3
Manish Gupta <manishiwa@gmail.com>
2023-11-16 01:44:04 -0800

    attempting bg gradient fix for newer ios devices

--- components/layout/Backgrounds.tsx
+++ components/layout/Backgrounds.tsx
@@ -31,7 +31,7 @@ export default function Backgrounds({ isMobileMenu }: BackgroundsProps) {
               right: "-6px",
               top: "0px",
               bottom: "602px",
-              backgroundImage: `-webkit-radial-gradient(45% 45% at 50% 50%, #FBB90D 0%, rgba(217, 217, 217, 0) 100%, rgba(251, 185, 13, 0) 100%)`,
+              background: `-webkit-radial-gradient(45% 45% at 50% 50%, #FBB90D 0%, rgba(217, 217, 217, 0) 100%, rgba(251, 185, 13, 0) 100%)`,
               background: `radial-gradient(45% 45% at 50% 50%, #FBB90D 0%, rgba(217, 217, 217, 0) 100%, rgba(251, 185, 13, 0) 100%)`,
             }}
             className="absolute z-0 mouse-events-none"
@@ -44,7 +44,7 @@ export default function Backgrounds({ isMobileMenu }: BackgroundsProps) {
               right: "-475px",
               top: "0px",
               bottom: "466px",
-              backgroundImage: `-webkit-radial-gradient(45% 45% at 50% 50%, #0DF6B9 0%, rgba(217, 217, 217, 0) 100%, rgba(13, 246, 185, 0) 100%)`,
+              background: `-webkit-radial-gradient(45% 45% at 50% 50%, #0DF6B9 0%, rgba(217, 217, 217, 0) 100%, rgba(13, 246, 185, 0) 100%)`,
               background: `radial-gradient(45% 45% at 50% 50%, #0DF6B9 0%, rgba(217, 217, 217, 0) 100%, rgba(13, 246, 185, 0) 100%)`,
             }}
             className="absolute z-0 mouse-events-none"
@@ -79,7 +79,7 @@ export default function Backgrounds({ isMobileMenu }: BackgroundsProps) {
             right: "-6px",
             top: "-90px",
             bottom: "602px",
-            backgroundImage: `-webkit-radial-gradient(45% 45% at 50% 50%, #FBB90D 0%, rgba(217, 217, 217, 0) 100%, rgba(251, 185, 13, 0) 100%)`,
+            background: `-webkit-radial-gradient(45% 45% at 50% 50%, #FBB90D 0%, rgba(217, 217, 217, 0) 100%, rgba(251, 185, 13, 0) 100%)`,
             background: `radial-gradient(45% 45% at 50% 50%, #FBB90D 0%, rgba(217, 217, 217, 0) 100%, rgba(251, 185, 13, 0) 100%)`,
           }}
           className="absolute z-0 mouse-events-none"
@@ -92,7 +92,7 @@ export default function Backgrounds({ isMobileMenu }: BackgroundsProps) {
             right: "-475px",
             top: "-90px",
             bottom: "466px",
-            backgroundImage: `-webkit-radial-gradient(45% 45% at 50% 50%, #0DF6B9 0%, rgba(217, 217, 217, 0) 100%, rgba(13, 246, 185, 0) 100%)`,
+            background: `-webkit-radial-gradient(45% 45% at 50% 50%, #0DF6B9 0%, rgba(217, 217, 217, 0) 100%, rgba(13, 246, 185, 0) 100%)`,
             background: `radial-gradient(45% 45% at 50% 50%, #0DF6B9 0%, rgba(217, 217, 217, 0) 100%, rgba(13, 246, 185, 0) 100%)`,
           }}
           className="absolute z-0 mouse-events-none"
