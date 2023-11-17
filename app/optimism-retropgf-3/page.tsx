c0902c7930386b9972ed1bde43b6f8f9bd40af17
Manish Gupta <manishiwa@gmail.com>
2023-11-16 23:20:54 -0800

    WIP: debugging

--- app/optimism-retropgf-3/page.tsx
+++ app/optimism-retropgf-3/page.tsx
@@ -3,7 +3,7 @@ import { ProjectsResponse } from "@/types/api/RetroPGF3";
 const URL =
   process.env.NEXT_PUBLIC_VERCEL_ENV === "development"
     ? `http://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/optimism-retropgf-3/projects`
-    : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/optimism-retropgf-3/projects`;
+    : `/api/optimism-retropgf-3/projects`;
 
 const getProjects = async (): Promise<ProjectsResponse> => {
   console.log("URL", URL);
