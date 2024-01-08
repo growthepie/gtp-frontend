28487c66c01ad9429c39e7eb29e8dece58e55c70
Manish Gupta <manishiwa@gmail.com>
2024-01-08 08:15:28 -0500

    hid notification section

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -16,7 +16,7 @@ export default function Header() {
           {/*Banner/Notification Area */}
 
           <div className={`hidden md:flex pr-[15px] `}>
-            <Notification />
+            {/* <Notification /> */}
           </div>
 
           <div className="flex justify-between items-start h-full md:hidden relative w-full">
@@ -42,7 +42,7 @@ export default function Header() {
             </Link>
 
             <div>
-              <Notification />
+              {/* <Notification /> */}
 
               <Sidebar isMobile={true} />
             </div>
@@ -96,9 +96,9 @@ export default function Header() {
         </div>
       </div>
       {process.env.NEXT_PUBLIC_VERCEL_ENV !== undefined &&
-      ["development", "preview"].includes(
-        process.env.NEXT_PUBLIC_VERCEL_ENV,
-      ) ? (
+        ["development", "preview"].includes(
+          process.env.NEXT_PUBLIC_VERCEL_ENV,
+        ) ? (
         <> </>
       ) : (
         <>{/* <SupportUsBanner /> */}</>
