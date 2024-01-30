e3554d004714d904f5f7e6d9c20c42ec51d6adbd
Manish Gupta <manishiwa@gmail.com>
2024-01-29 19:37:06 -0800

    add million

---
+++ next.config.mjs
@@ -0,0 +1,26 @@
+import million from "million/compiler";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    // domains: ["ipfs.io", "content.optimism.io"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ipfs.io",
+      },
+      {
+        protocol: "https",
+        hostname: "content.optimism.io",
+      },
+    ],
+  },
+};
+
+const millionConfig = {
+  auto: true,
+};
+
+// module.exports = nextConfig;
+
+export default million.next(nextConfig, millionConfig);
