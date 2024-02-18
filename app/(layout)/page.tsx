101ef2a67a3e1f7ce2233298682cc2ad423b0379
Manish Gupta <manishiwa@gmail.com>
2024-02-18 13:57:55 -0800

    Update page.tsx

--- app/(layout)/page.tsx
+++ app/(layout)/page.tsx
@@ -15,7 +15,7 @@ import Image from "next/image";
 
 export async function generateMetadata(): Promise<Metadata> {
   return {
-    title: "Growing Ethereum’s Ecosystem Together - Layer 2 User Base",
+    title: "growthepie - Growing Ethereum’s Ecosystem Together - Layer 2 User Base",
     description:
       "At growthepie, our mission is to provide comprehensive and accurate analytics of layer 2 solutions for the Ethereum ecosystem, acting as a trusted data aggregator from reliable sources such as L2Beat and DefiLlama, while also developing our own metrics.",
   };
