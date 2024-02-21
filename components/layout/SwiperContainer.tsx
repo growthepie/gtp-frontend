cbb158234c75e4c2a9bf395bb5e1bb7da1611dbc
mokelgit <maym2018@gmail.com>
2024-02-21 12:28:31 -0500

    added Market Cap to swiper on home page

--- components/layout/SwiperContainer.tsx
+++ components/layout/SwiperContainer.tsx
@@ -70,15 +70,16 @@ export default function SwiperContainer({ ariaId }: { ariaId?: string }) {
         // }}
       >
         <SplideTrack>
-          {["txcount", "stables_mcap", "fees", "rent_paid"].map((metric_id) => (
-            <SplideSlide key={metric_id}>
-              <div
-                className="group w-full chain relative"
-                // style={{
-                //   pointerEvents: isDragging ? "none" : "all",
-                // }}
-              >
-                {/* <Suspense
+          {["txcount", "stables_mcap", "fees", "rent_paid", "market_cap"].map(
+            (metric_id) => (
+              <SplideSlide key={metric_id}>
+                <div
+                  className="group w-full chain relative"
+                  // style={{
+                  //   pointerEvents: isDragging ? "none" : "all",
+                  // }}
+                >
+                  {/* <Suspense
               fallback={
                 <div className="w-full h-[145px] md:h-[183px] rounded-[15px]">
                   <div
@@ -96,25 +97,26 @@ export default function SwiperContainer({ ariaId }: { ariaId?: string }) {
             >
               <SwiperItem metric_id={metric_id} landing={landing} />
             </Suspense> */}
-                {landing ? (
-                  <SwiperItem metric_id={metric_id} landing={landing} />
-                ) : (
-                  <div className="w-full h-[145px] md:h-[183px] rounded-[15px]">
-                    <div
-                      role="status"
-                      className="flex items-center justify-center h-full w-full rounded-lg animate-pulse bg-forest-50 dark:bg-[#1F2726]"
-                    >
-                      <Icon
-                        icon="feather:loading"
-                        className="w-6 h-6 text-white z-10"
-                      />
-                      <span className="sr-only">Loading...</span>
+                  {landing ? (
+                    <SwiperItem metric_id={metric_id} landing={landing} />
+                  ) : (
+                    <div className="w-full h-[145px] md:h-[183px] rounded-[15px]">
+                      <div
+                        role="status"
+                        className="flex items-center justify-center h-full w-full rounded-lg animate-pulse bg-forest-50 dark:bg-[#1F2726]"
+                      >
+                        <Icon
+                          icon="feather:loading"
+                          className="w-6 h-6 text-white z-10"
+                        />
+                        <span className="sr-only">Loading...</span>
+                      </div>
                     </div>
-                  </div>
-                )}
-              </div>
-            </SplideSlide>
-          ))}
+                  )}
+                </div>
+              </SplideSlide>
+            ),
+          )}
         </SplideTrack>
 
         <div className="splide__arrows -mt-8 md:-mt-0">
