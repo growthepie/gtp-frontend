c3784add0644035ca49d22d22124260b745f7939
Manish Gupta <manishiwa@gmail.com>
2024-02-29 10:50:44 -0500

    Landing top charts - mobile gradient line fix

--- components/charts/ChainComponent.tsx
+++ components/charts/ChainComponent.tsx
@@ -230,7 +230,7 @@ export default function ChainComponent({
     Object.keys(data.metrics).forEach((key) => {
       maxUnixtimes.push(
         data.metrics[key].daily.data[
-          data.metrics[key].daily.data.length - 1
+        data.metrics[key].daily.data.length - 1
         ][0],
       );
     });
@@ -448,11 +448,11 @@ export default function ChainComponent({
       const dateString = `
       <div>
         ${date.toLocaleDateString(undefined, {
-          timeZone: "UTC",
-          month: "short",
-          day: "numeric",
-          year: "numeric",
-        })}
+        timeZone: "UTC",
+        month: "short",
+        day: "numeric",
+        year: "numeric",
+      })}
       </div>
       `;
 
@@ -474,28 +474,25 @@ export default function ChainComponent({
           if (selectedScale === "percentage")
             return `
               <div class="flex w-full space-x-2 items-center font-medium mb-1">
-                <div class="w-4 h-1.5 rounded-r-full" style="background-color: ${
-                  AllChainsByKeys[data.chain_id].colors[theme ?? "dark"][0]
-                }"></div>
+                <div class="w-4 h-1.5 rounded-r-full" style="background-color: ${AllChainsByKeys[data.chain_id].colors[theme ?? "dark"][0]
+              }"></div>
                 <!--
-                <div class="tooltip-point-name">${
-                  AllChainsByKeys[data.chain_id].label
-                }</div>
+                <div class="tooltip-point-name">${AllChainsByKeys[data.chain_id].label
+              }</div>
                 -->
                 <div class="flex-1 text-right font-inter">${Highcharts.numberFormat(
-                  percentage,
-                  2,
-                )}%</div>
+                percentage,
+                2,
+              )}%</div>
               </div>
               <!-- <div class="flex ml-6 w-[calc(100% - 24rem)] relative mb-1">
                 <div class="h-[2px] w-full bg-gray-200 rounded-full absolute left-0 top-0" > </div>
 
                 <div class="h-[2px] rounded-full absolute left-0 top-0" style="width: ${Highcharts.numberFormat(
-                  percentage,
-                  2,
-                )}%; background-color: ${
-              AllChainsByKeys[data.chain_id].colors[theme ?? "dark"][0]
-            };"> </div>
+                percentage,
+                2,
+              )}%; background-color: ${AllChainsByKeys[data.chain_id].colors[theme ?? "dark"][0]
+              };"> </div>
               </div> -->`;
 
           let prefix = displayValues[series.name].prefix;
@@ -514,25 +511,21 @@ export default function ChainComponent({
 
           return `
           <div class="flex w-full space-x-2 items-center font-medium mb-1">
-            <div class="w-4 h-1.5 rounded-r-full" style="background-color: ${
-              AllChainsByKeys[data.chain_id].colors[theme ?? "dark"][0]
+            <div class="w-4 h-1.5 rounded-r-full" style="background-color: ${AllChainsByKeys[data.chain_id].colors[theme ?? "dark"][0]
             }"></div>
             <!--
-            <div class="tooltip-point-name text-md">${
-              AllChainsByKeys[data.chain_id].label
+            <div class="tooltip-point-name text-md">${AllChainsByKeys[data.chain_id].label
             }</div>
             -->
             <div class="flex-1 text-left justify-start font-inter flex">
-                <div class="opacity-70 mr-0.5 ${
-                  !prefix && "hidden"
-                }">${prefix}</div>
+                <div class="opacity-70 mr-0.5 ${!prefix && "hidden"
+            }">${prefix}</div>
                 ${parseFloat(value).toLocaleString(undefined, {
-                  minimumFractionDigits: prefix ? 2 : 0,
-                  maximumFractionDigits: prefix ? 2 : 0,
-                })}
-                <div class="opacity-70 ml-0.5 ${
-                  !suffix && "hidden"
-                }">${suffix}</div>
+              minimumFractionDigits: prefix ? 2 : 0,
+              maximumFractionDigits: prefix ? 2 : 0,
+            })}
+                <div class="opacity-70 ml-0.5 ${!suffix && "hidden"
+            }">${suffix}</div>
             </div>
           </div>
           <!-- <div class="flex ml-4 w-[calc(100% - 1rem)] relative mb-1">
@@ -541,9 +534,8 @@ export default function ChainComponent({
             <div class="h-[2px] rounded-full absolute right-0 top-0" style="width: ${formatNumber(
               name,
               (y / pointsSum) * 100,
-            )}%; background-color: ${
-            AllChainsByKeys[data.chain_id].colors[theme ?? "dark"][0]
-          }33;"></div>
+            )}%; background-color: ${AllChainsByKeys[data.chain_id].colors[theme ?? "dark"][0]
+            }33;"></div>
           </div> -->`;
         })
         .join("");
@@ -1071,9 +1063,9 @@ export default function ChainComponent({
     lastPointLines[i][lastPointLines[i].length] = chart.renderer
       .createElement("line")
       .attr({
-        x1: chart.chartWidth * (1 - fraction) + 0.00005,
+        x1: chart.chartWidth * (1 - fraction) + 0.0005,
         y1: lastPoint.plotY ? lastPoint.plotY + chart.plotTop : 0,
-        x2: chart.chartWidth * (1 - fraction) - 0.00005,
+        x2: chart.chartWidth * (1 - fraction) - 0.0005,
         y2: chart.plotTop / 2,
         stroke: "url('#gradient0')",
         "stroke-dasharray": "2",
@@ -1085,9 +1077,9 @@ export default function ChainComponent({
     lastPointLines[i][lastPointLines[i].length] = chart.renderer
       .createElement("line")
       .attr({
-        x1: chart.chartWidth * (1 - fraction) + 0.5,
+        x1: chart.chartWidth * (1 - fraction) + 0.0005,
         y1: chart.plotTop / 2 + 0.00005,
-        x2: chart.chartWidth * (1 - fraction),
+        x2: chart.chartWidth * (1 - fraction) - 0.0005,
         y2: chart.plotTop / 2,
         stroke: AllChainsByKeys[data.chain_id].colors[theme ?? "dark"][1],
         "stroke-dasharray": 2,
@@ -1269,27 +1261,27 @@ export default function ChainComponent({
                   data: data.metrics[category].daily.types.includes("eth")
                     ? showUsd
                       ? data.metrics[category].daily.data.map((d) => [
-                          d[0],
-                          d[data.metrics[category].daily.types.indexOf("usd")],
-                        ])
-                      : data.metrics[category].daily.data.map((d) => [
-                          d[0],
-                          showGwei(category)
-                            ? d[
-                                data.metrics[category].daily.types.indexOf(
-                                  "eth",
-                                )
-                              ] * 1000000000
-                            : d[
-                                data.metrics[category].daily.types.indexOf(
-                                  "eth",
-                                )
-                              ],
-                        ])
-                    : data.metrics[category].daily.data.map((d) => [
                         d[0],
-                        d[1],
-                      ]),
+                        d[data.metrics[category].daily.types.indexOf("usd")],
+                      ])
+                      : data.metrics[category].daily.data.map((d) => [
+                        d[0],
+                        showGwei(category)
+                          ? d[
+                          data.metrics[category].daily.types.indexOf(
+                            "eth",
+                          )
+                          ] * 1000000000
+                          : d[
+                          data.metrics[category].daily.types.indexOf(
+                            "eth",
+                          )
+                          ],
+                      ])
+                    : data.metrics[category].daily.data.map((d) => [
+                      d[0],
+                      d[1],
+                    ]),
                   showInLegend: false,
                   marker: {
                     enabled: false,
@@ -1327,11 +1319,11 @@ export default function ChainComponent({
                         attributes: {
                           fill:
                             AllChainsByKeys[data.chain_id]?.colors[
-                              theme ?? "dark"
+                            theme ?? "dark"
                             ][0] + "99",
                           stroke:
                             AllChainsByKeys[data.chain_id]?.colors[
-                              theme ?? "dark"
+                            theme ?? "dark"
                             ][0] + "66",
                           strokeWidth: 0,
                         },
