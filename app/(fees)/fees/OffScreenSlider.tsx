a4264d1997937eb5dba696c869f64fb4c31ae2a3
Manish Gupta <manishiwa@gmail.com>
2024-04-12 21:42:28 -0400

    fees changes

--- app/(fees)/fees/OffScreenSlider.tsx
+++ app/(fees)/fees/OffScreenSlider.tsx
@@ -1,17 +1,19 @@
 import FeesContainer from "@/components/layout/FeesContainer";
+import React from "react";
 
 type SlidingFooterContainerProps = {
   children: React.ReactNode;
 };
 
-export default function OffScreenSlider({
-  children,
-}: SlidingFooterContainerProps) {
+export default React.forwardRef(function OffScreenSlider(
+  { children }: SlidingFooterContainerProps,
+  ref: React.Ref<HTMLDivElement>
+) {
   return (
-    <div className={"fixed w-full max-w-[650px] md:max-w-[900px] mx-auto bottom-0"}>
+    <div className={"fixed w-full max-w-[650px] md:max-w-[900px] mx-auto bottom-0"} ref={ref}>
       <FeesContainer>
         {children}
       </FeesContainer>
     </div>
   );
-}
+});
