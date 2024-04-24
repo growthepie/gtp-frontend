30ab574346f5a18573970a6b24d053b2a7ce4781
mokelgit <maym2018@gmail.com>
2024-04-24 11:05:23 -0400

    adding alternative preview

--- app/(layout)/chains/[chain]/layout.tsx
+++ app/(layout)/chains/[chain]/layout.tsx
@@ -38,6 +38,12 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
       openGraph: {
         images: [
           {
+            url: `http://api.growthepie.xyz/v1/og_images/chains/${replaceKey}.png?date=${dateString}&size=small`,
+            width: 600,
+            height: 314,
+            alt: "growthepie.xyz",
+          },
+          {
             url: `http://api.growthepie.xyz/v1/og_images/chains/${replaceKey}.png?date=${dateString}`,
             width: 1200,
             height: 627,
