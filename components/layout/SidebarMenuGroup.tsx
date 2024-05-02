f843ef5f0e86b3579d65025fcd80fc46f604b8ee
Manish Gupta <manishiwa@gmail.com>
2024-05-01 20:00:08 -0700

    sidebar nav - "new" shows on parent, on children when expanded

--- components/layout/SidebarMenuGroup.tsx
+++ components/layout/SidebarMenuGroup.tsx
@@ -432,6 +432,17 @@ export default function SidebarMenuGroup({
                 </div>
               </div>
             )}
+            {item.options.some((option) => option.showNew) && (
+              <div
+                className={`transition-opacity duration-300 relative top-0 right-4 rounded-full text-xs flex items-center justify-center font-bold bg-gradient-to-b from-[#FE5468] to-[#FFDF27] p-[1px] ${
+                  isOpen ? "opacity-0" : "opacity-100"
+                }`}
+              >
+                <div className="rounded-full text-xs flex items-center justify-center font-bold  px-2 py-[1px] hard-shine-2 text-white dark:text-forest-900 ">
+                  NEW!
+                </div>
+              </div>
+            )}
           </div>
         </TooltipTrigger>
         {!sidebarOpen && (
@@ -543,7 +554,13 @@ export default function SidebarMenuGroup({
                         </div>
                       )}
                       {option.showNew && (
-                        <div className="relative top-0 right-7 rounded-full text-xs flex items-center justify-center font-bold bg-gradient-to-b from-[#FE5468] to-[#FFDF27] p-[1px]">
+                        <div
+                          className={`transition-opacity duration-300 relative top-0 right-7 rounded-full text-xs flex items-center justify-center font-bold bg-gradient-to-b from-[#FE5468] to-[#FFDF27] p-[1px] ${
+                            isOpen
+                              ? "opacity-100 delay-50"
+                              : "opacity-0 delay-0"
+                          }`}
+                        >
                           <div className="rounded-full text-xs flex items-center justify-center font-bold  px-2 py-[1px] hard-shine-2 text-white dark:text-forest-900 ">
                             NEW!
                           </div>
