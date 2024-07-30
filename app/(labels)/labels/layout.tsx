27f3eba724950ef7253db1b69d6e9a9c71fc7dc9
Manish Gupta <manishiwa@gmail.com>
2024-07-30 11:05:30 -0400

    labels - remove dev tools

--- app/(labels)/labels/layout.tsx
+++ app/(labels)/labels/layout.tsx
@@ -114,7 +114,7 @@ export default function RootLayout({
               </div> */}
             </main>
           </MasterProvider>
-          <DeveloperTools />
+          {/* <DeveloperTools /> */}
           <CookieConsent />
         </Providers >
       </body >
