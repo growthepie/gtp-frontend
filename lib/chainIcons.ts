2e2dbbfc786ccb3b51fd88036d37682af84ff4ff
Manish Gupta <manishiwa@gmail.com>
2024-08-15 11:15:36 -0400

    master - chain icon fix

--- lib/chainIcons.ts
+++ lib/chainIcons.ts
@@ -15,6 +15,13 @@ export const ImportChainIcons = (master: MasterResponse) => {
     if (!chainInfo.logo) return;
 
     // add icon with key as chainKey
+    chainIcons.icons[`${chainInfo.url_key}-logo-monochrome`] = {
+      body: chainInfo.logo.body,
+      width: chainInfo.logo.width || 15,
+      height: chainInfo.logo.height || 15,
+    };
+
+    // add icon with key as chainKey
     chainIcons.icons[`${chainKey.replace("_", "-")}-logo-monochrome`] = {
       body: chainInfo.logo.body,
       width: chainInfo.logo.width || 15,
