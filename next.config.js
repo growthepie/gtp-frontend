2494c05e7a7c7b324162d54eadc1c89905fd9e44
mokelgit <maym2018@gmail.com>
2024-10-11 11:49:28 -0400

    Added redirects changed route to donate instead of donations

--- next.config.js
+++ next.config.js
@@ -68,6 +68,36 @@ const nextConfig = {
         destination: "/trackers/octant",
         permanent: true,
       },
+      {
+        source: "/donategiveth",
+        destination:
+          "https://giveth.io/project/growthepie-analytics-for-ethereum-scaling-solutions",
+        permanent: true,
+      },
+      {
+        source: "/donateeth",
+        destination:
+          "https://etherscan.io/address/0x7291a5Aa55886900C460Bf4366A46820F40476fB",
+        permanent: true,
+      },
+      {
+        source: "/donateop",
+        destination:
+          "https://optimistic.etherscan.io/address/0x700E73d289DE10b6143465E02E6931E6e6a0CA15",
+        permanent: true,
+      },
+      {
+        source: "/donatebase",
+        destination:
+          "https://basescan.org/address/0xC9847131b6acb8dA9BfC4Ec970Be1D604215A1E4",
+        permanent: true,
+      },
+      {
+        source: "/donatearb",
+        destination:
+          "https://arbiscan.io/address/0x18f79D6d2166997c9A237C25c4692647CD4faf59",
+        permanent: true,
+      },
     ];
   },
   images: {
