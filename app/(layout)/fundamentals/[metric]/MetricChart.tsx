8025bbe0ef8f40642e360b18cad2517b95b04973
Manish Gupta <manishiwa@gmail.com>
2024-10-27 20:15:52 -0400

    add content-container class to da/fund metrics pages

--- app/(layout)/fundamentals/[metric]/MetricChart.tsx
+++ app/(layout)/fundamentals/[metric]/MetricChart.tsx
@@ -1056,7 +1056,7 @@ function MetricChart({
         </>
 
       </div>
-      <div className="relative w-full h-full overflow-visible" ref={containerRef}>
+      <div id="content-container" className="relative w-full h-full overflow-visible" ref={containerRef}>
         <div className="absolute w-[2px] bottom-[71px] left-[38.5px] -top-[23px]" style={{
           backgroundImage: `url("data:image/svg+xml,%3Csvg width='2' height='396' viewBox='0 0 2 396' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cg clip-path='url(%23clip0_16047_48574)'%3E%3Cpath opacity='0.5' d='M1.00002 396L1 0' stroke='%235A6462' stroke-width='2' stroke-dasharray='3 6'/%3E%3C/g%3E%3Cdefs%3E%3CclipPath id='clip0_16047_48574'%3E%3Crect width='2' height='396' fill='white'/%3E%3C/clipPath%3E%3C/defs%3E%3C/svg%3E%0A")`,
         }} />
