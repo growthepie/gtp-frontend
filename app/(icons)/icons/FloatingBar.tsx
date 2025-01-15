d9c6fdc5bb88de891b5cf8a7413ec79aca57e0c6
naderbennour <naderbennour@outlook.com>
2025-01-15 10:23:44 +0100

    SVG selected by default in icons download

--- app/(icons)/icons/FloatingBar.tsx
+++ app/(icons)/icons/FloatingBar.tsx
@@ -16,7 +16,7 @@ export default function FloatingBar({
   setSearchQuery,
   iconsCount,
 }: FloatingBarProps) {
-  const [selectedFormat, setSelectedFormat] = useState<"SVG" | "PNG" | null>(null);
+  const [selectedFormat, setSelectedFormat] = useState<"SVG" | "PNG">("SVG");
 
   return (
     <div className="flex p-[5px] items-center w-full rounded-full mt-[16px] bg-[#344240] shadow-[0px_0px_50px_0px_#000000] gap-x-[5px] md:gap-x-[15px] z-0 pointer-events-auto">
