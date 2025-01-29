268e87383007a39a0b215c1c4d542d64f999c111
mokelgit <maym2018@gmail.com>
2025-01-29 09:25:54 -0500

    disabling panning on headcharts

--- components/layout/DA-Overview/DAHeadCharts.tsx
+++ components/layout/DA-Overview/DAHeadCharts.tsx
@@ -467,7 +467,7 @@ export default function DAHeadCharts({selectedTimespan, isMonthly, data}: {selec
                                 backgroundColor={"transparent"}
                                 type="area"
                                 title={"test"}
-                                panning={{ enabled: true }}
+                                panning={{ enabled: false }}
                                 panKey="shift"
                                 zooming={{ type: undefined }}
                                 style={{ borderRadius: 15 }}
