000dd30075f0c8f56d6f1dfe1c85318f1a02bd34
mokelgit <maym2018@gmail.com>
2025-02-28 12:45:16 -0500

    trying to trigger fix on dev

--- app/(layout)/page.tsx
+++ app/(layout)/page.tsx
@@ -24,7 +24,7 @@ export async function generateMetadata(): Promise<Metadata> {
       "At growthepie, our mission is to provide comprehensive and accurate analytics of layer 2 solutions for the Ethereum ecosystem, acting as a trusted data aggregator from reliable sources such as L2Beat and DefiLlama, while also developing our own metrics.",
   };
 }
-
+/** */
 export default async function Page() {
   return (
     <>
