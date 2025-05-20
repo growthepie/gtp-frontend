7fca6a7ba34352d16fd7c31c84195682790f3b4c
mokelgit <maym2018@gmail.com>
2025-05-20 13:47:08 -0400

    fixing issues callout blocks

--- components/quick-bites/blocks/CalloutBlock.tsx
+++ components/quick-bites/blocks/CalloutBlock.tsx
@@ -9,14 +9,14 @@ interface CalloutBlockProps {
 
 export const CalloutBlock: React.FC<CalloutBlockProps> = ({ block }) => {
   return (
-    <div className={`my-[15px] p-[15px] bg-forest-50 dark:bg-forest-900 rounded-[15px] border-l-4 text-xs md:text-sm leading-[1.5] ${block.color ? `border-${"[#E3E8E7]"}` : 'border-[#E3E8E7]'} ${block.className || ''}`}>
+    <div className={`my-6 p-[15px] bg-forest-50 dark:bg-forest-900 rounded-[25px] md:rounded-lg border-l-4 text-xs md:text-sm md:w-auto w-full ${block.color ? `border-${"[#5A6462]"}` : 'border-[#5A6462]'} ${block.className || ''}`}>
       <div className="flex items-start gap-3">
         {block.icon && (
           <div className="flex-shrink-0 mt-1">
             <GTPIcon icon={block.icon as any} size="sm" />
           </div>
         )}
-        <div dangerouslySetInnerHTML={{ __html: block.content }} />
+        <div dangerouslySetInnerHTML={{ __html: block.content }}  />
       </div>
     </div>
   );
