5dc8f082a651b1b3e014834778faf6344622f4b8
Matthias Seidl <matthias@mseidl-analytics.de>
2025-05-30 11:47:55 +0200

    small timeboost qb changes

--- lib/quick-bites/qb-arbitrum-timeboost.ts
+++ lib/quick-bites/qb-arbitrum-timeboost.ts
@@ -8,7 +8,7 @@ const arbitrumTimeboost: QuickBiteData = {
 
     "# Timeboost Usage",
     "The introduction of Arbitrum Timeboost has been a great success. It started to generate significant revenue from day one.",
-    "> Total DAO Revenue from Timeboost on Arbitrum to date: ${{timeboostTotalUSD}} or {{timeboostTotalETH}} ETH.",
+    "> Total DAO Revenue from Timeboost on Arbitrum to date: ${{timeboostTotalUSD}} or Ξ{{timeboostTotalETH}} (this number updates daily).",
     
     "```chart",
     JSON.stringify({
