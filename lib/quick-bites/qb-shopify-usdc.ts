ab37d0e350915843ede3ae3c253a63f28addef85
GitWave22 <135379882+GitWave22@users.noreply.github.com>
2025-06-26 12:22:04 +0100

    qb-shopify: reorder and improved images

--- lib/quick-bites/qb-shopify-usdc.ts
+++ lib/quick-bites/qb-shopify-usdc.ts
@@ -7,12 +7,7 @@ const shopifyUsdc: QuickBiteData = {
 
     'On June 12, 2025, Shopify announced a landmark integration with Coinbase and Stripe. This enables USDC payments on Base to "bring frictionless, secure stablecoin payments to merchants around the world".',
     "> USDC is a token that is pegged to the value of the US dollar, backed by treasuries (commonly known as stablecoins). It has over $60 Billion in circulation and is one of the most widely adopted stablecoins ranking in the top 10 for all crypto assets by market cap. ",
-    "## Why Shopify Chose USDC/Base:",
-    "- Faster - Payments on Base can settle in as little as 200 milliseconds across international borders, improving cashflow management.",
-    "- Cheaper - Transaction fees on Base are significantly cheaper and are independent of the value of goods sold, improving profit margins.",
-    "- Globally Accessible - Opening up new markets with a onchain, internet native currency, improving market penetration.",
-    "- Composable - Reducing complexity and middlemen by using a single, shared source of truth, improving operational efficiency.",
-    "Note: These benefits are largely merchant focused with traditional card payment networks often blocking alternatives from providing discounts, this may make changing current customer behavior challenging. For emerging economies with less access to traditional payment options USDC may prove to be highly favorable and this could be part of Shopify's USDC strategy.",
+  
     "# Shopify's USDC Adoption Metrics (All-Time)",
 
     "```kpi-cards",JSON.stringify(
@@ -154,24 +149,30 @@ const shopifyUsdc: QuickBiteData = {
       seeMetricURL: null
     }),
     "```",
-
+    "# Why Shopify Chose USDC/Base:",
+    "- Faster - Payments on Base can settle in as little as 200 milliseconds across international borders, improving cashflow management.",
+    "- Cheaper - Transaction fees on Base are significantly cheaper and are independent of the value of goods sold, improving profit margins.",
+    "- Globally Accessible - Opening up new markets with a onchain, internet native currency, improving market penetration.",
+    "- Composable - Reducing complexity and middlemen by using a single, shared source of truth, improving operational efficiency.",
+    "Note: These benefits are largely merchant focused with traditional card payment networks often blocking alternatives from providing discounts, this may make changing current customer behavior challenging. For emerging economies with less access to traditional payment options USDC may prove to be highly favorable and this could be part of Shopify's USDC strategy.",
+    
     "# Implementation",
     'The integration is powered by the new Commerce Payments Protocol, an open-source framework developed by Coinbase. Its core innovation is an onchain escrow system that mimics the "authorize and capture" flow common in traditional finance.',
     "```image",
     JSON.stringify({
-      src: "https://api.growthepie.com/v1/quick-bites/shopify-payment-operations.png", // should allow link to our API
+      src: "https://api.growthepie.com/v1/quick-bites/shopify/shopify-payment-operations.png", // should allow link to our API
       alt: "6 core payment operations outlined by Shopify",
-      width: "600",
-      height: "330",
+      width: "687",
+      height: "390",
       caption: "6 core payment operations outlined by Shopify",
     }),
     "```",
     "```image",
     JSON.stringify({
-      src: "https://api.growthepie.com/v1/quick-bites/shopify-payment-example.png", // should allow link to our API
+      src: "https://api.growthepie.com/v1/quick-bites/shopify/shopify-payment-example.png", // should allow link to our API
       alt: "Example payment flow illustrated by Shopify",
-      width: "600",
-      height: "600",
+      width: "638",
+      height: "647",
       caption: "Example payment flow illustrated by Shopify",
     }),
     "```",
