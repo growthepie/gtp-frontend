84717299b747bc5eb6cdd7a886efea798bb64f6a
Manish Gupta <manishiwa@gmail.com>
2025-07-04 13:49:25 -0700

    [eth agg] menu changes

--- components/layout/EthAgg/TopSelectArea.tsx
+++ components/layout/EthAgg/TopSelectArea.tsx
@@ -24,7 +24,7 @@ export default function TopSelectArea({
                 <ComingSoonButton selectedBreakdownGroup={selectedBreakdownGroup} setSelectedBreakdownGroup={setSelectedBreakdownGroup} breakdownGroup="Ethereum Ecosystem" icon="gtp-ethereumlogo-monochrome" />
                 <ComingSoonButton selectedBreakdownGroup={selectedBreakdownGroup} setSelectedBreakdownGroup={setSelectedBreakdownGroup} breakdownGroup="Builders & Apps" icon="gtp-project-monochrome" />
             </div>
-            <div className="absolute bottom-[0px] left-0 w-full h-[34px] z-[-1] bg-[#1F2726] rounded-full mr-[10px]"></div>
+            <div className="absolute bottom-[2px] left-0 w-full h-[24px] z-[-1] bg-[#1F2726] rounded-full mr-[10px]"></div>
         </div>
     </Container>
   );
@@ -43,7 +43,7 @@ const ComingSoonButton = ( {
 }) => {
    
     return (
-        <div className={`flex items-center justify-between w-full flex-1 flex-row gap-x-[15px] rounded-full px-[10px] py-[3px] transition-all text-[#344240]
+        <div className={`flex ${selectedBreakdownGroup === breakdownGroup && "flex-1"} items-center justify-between flex-row gap-x-[15px] rounded-full px-[10px] py-[3px] transition-all text-[#344240]
             ${selectedBreakdownGroup === breakdownGroup ? "bg-active-black border-[0px] border-[#1F2726] h-[44px] " 
                                         : "bg-[#5A6462] h-[36px]"}`}
             onClick={() => !IS_PRODUCTION ? setSelectedBreakdownGroup(breakdownGroup) : null}
@@ -54,7 +54,7 @@ const ComingSoonButton = ( {
         >
             <div className="flex items-center gap-x-[15px]">
                 <GTPIcon icon={icon} size={"md"} className={`transition-all relative ${breakdownGroup === "Builders & Apps" ? "bottom-[2px] " : ""}`}/>
-                <div className={`${"heading-large-sm"} transition-all text-nowrap `}>{breakdownGroup}</div>
+                <div className={`${selectedBreakdownGroup === breakdownGroup && "!block" } hidden lg:block ${"heading-large-sm"} transition-all text-nowrap `}>{breakdownGroup}</div>
             </div>
             <div className="flex items-center py-[2px] px-[10px] bg-gradient-to-b from-[#FE5468] to-[#FFDF27] rounded-full">
                 <div className={`${"heading-large-sm"} transition-all text-nowrap heading-small-xxxs `}>SOON</div>
@@ -77,8 +77,8 @@ const SelectionButton = ({
 
     const [isHovered, setIsHovered] = useState(false);
     return (
-        <div className={`flex items-center justify-start w-full flex-1 flex-row gap-x-[15px] rounded-full px-[10px] py-[3px] transition-all cursor-pointer
-            ${selectedBreakdownGroup === breakdownGroup ? "bg-active-black border-[3px] border-[#1F2726] h-[44px]" 
+        <div className={`flex ${selectedBreakdownGroup === breakdownGroup && "flex-1"} justify-start flex-row gap-x-[15px] rounded-full px-[10px] py-[3px] transition-all cursor-pointer
+            ${selectedBreakdownGroup === breakdownGroup ? "bg-active-black border-[3px] border-[#344240] h-[44px]" 
                                         : "bg-medium-background hover:bg-[#5A6462] h-[36px] hover:h-[44px]"}`}
             onClick={() => setSelectedBreakdownGroup(breakdownGroup)}
             style={{
@@ -86,9 +86,11 @@ const SelectionButton = ({
             }}
             onMouseEnter={() => setIsHovered(true)}
             onMouseLeave={() => setIsHovered(false)}
-        >
-            <GTPIcon icon={icon} size={(breakdownGroup === selectedBreakdownGroup || isHovered) ? "lg" : "md"} className={`transition-all relative ${breakdownGroup === "Builders & Apps" ? "bottom-[2px]" : ""}`}/>
-            <div className={`${(selectedBreakdownGroup === breakdownGroup || isHovered) ? "heading-large-md" : "heading-large-sm"} transition-all text-nowrap`}>{breakdownGroup}</div>
+        >   
+            <div className={`flex items-center gap-x-[15px] ${isHovered || selectedBreakdownGroup === breakdownGroup ? "scale-[122%]" : "scale-100"} origin-left transition-all text-nowrap`}>
+                <GTPIcon icon={icon} size="md" className={`transition-all relative ${breakdownGroup === "Builders & Apps" ? "bottom-[2px]" : ""}`}/>
+                <div className={`${selectedBreakdownGroup === breakdownGroup && "!block pr-[30px]" } hidden lg:block lg:pr-[30px] heading-large-sm transition-all text-nowrap`}>{breakdownGroup}</div>
+            </div>
         </div>
     );
 }
