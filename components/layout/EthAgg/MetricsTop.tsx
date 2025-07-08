26d8e57425cdf7301165e0d5342e0172d6bf1d82
Manish Gupta <manishiwa@gmail.com>
2025-07-08 08:47:38 -0700

    [eth agg] remove max from tps

--- components/layout/EthAgg/MetricsTop.tsx
+++ components/layout/EthAgg/MetricsTop.tsx
@@ -384,8 +384,8 @@ export const EthereumEcosystemTPSCard = React.memo(({
             {isCompact && <div className='heading-small-xs text-[#5A6462] pt-[5px] h-0 overflow-visible'>all chains combined</div>}
           </div>
 
-          <div className={`justify-between ${isCompact ? 'hidden' : 'flex w-[73%]'}`}>
-            <div className='numbers-xs flex items-center gap-x-1'><span className='text-xs'>Max (24h):</span>{activeGlobalMetrics.total_tps_24h_high?.toLocaleString("en-GB", { minimumFractionDigits: 0, maximumFractionDigits: 0 }) || 0} TPS</div>
+          <div className={`justify-end ${isCompact ? 'hidden' : 'flex'}`}>
+            {/* <div className='numbers-xs flex items-center gap-x-1'><span className='text-xs'>Max (24h):</span>{activeGlobalMetrics.total_tps_24h_high?.toLocaleString("en-GB", { minimumFractionDigits: 0, maximumFractionDigits: 0 }) || 0} TPS</div> */}
             <div className='numbers-xs flex items-center gap-x-1'><span className='text-xs'>ATH:</span>{activeGlobalMetrics.total_tps_ath?.toLocaleString("en-GB", { minimumFractionDigits: 0, maximumFractionDigits: 0 }) || 0} TPS</div>
           </div>
         </div>
