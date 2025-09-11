922ec6483dbbc29e2f62901032a5235d474a3ea5
lorenz234 <lorenz_lehmann@yahoo.de>
2025-09-11 12:19:40 +0200

    remove unused qb

--- lib/quick-bites/index.ts
+++ lib/quick-bites/index.ts
@@ -7,7 +7,6 @@ import arbitrumTimeboost from './qb-arbitrum-timeboost';
 import shopifyUsdc from './qb-shopify-usdc';
 import robinhoodStock from './qb-robinhood-stock';
 import anniversaryReport from './qb-anniversary-report';
-import hyperliquidUSDC from './qb-hyperliquid';
 import arbitrumHyperliquidBridge from './qb-arbitrum-hyperliquid-bridge';
 
 const QUICK_BITES_DATA: Record<string, QuickBiteData> = {
@@ -17,7 +16,6 @@ const QUICK_BITES_DATA: Record<string, QuickBiteData> = {
   "base-commerce": shopifyUsdc,
   "robinhood-stock": robinhoodStock,
   "anniversary-report": anniversaryReport,
-  "hyperliquid-usdc": hyperliquidUSDC,
   "arbitrum-hyperliquid-bridge": arbitrumHyperliquidBridge,
 };
 
