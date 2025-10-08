aabd9a85732b396cfa415c7a80b985e12b916f1a
GitWave22 <135379882+GitWave22@users.noreply.github.com>
2025-10-08 16:08:20 +0100

    related quickbites spacing

--- components/quick-bites/QuickBiteCard.tsx
+++ components/quick-bites/QuickBiteCard.tsx
@@ -78,7 +78,7 @@ const QuickBiteCard: React.FC<QuickBiteCardProps> = ({
               <time dateTime={date}>{formatDate(date)}</time>
             </div>
           </div>
-          <div className='text-xs h-[30px]'>{subtitle}</div>
+          <div className='text-xxs h-[30px]'>{subtitle}</div>
         </div>
         <div className='flex flex-1 justify-between items-center gap-x-[15px]'>
           <div className='relative bg-color-ui-hover rounded-[15px] w-full h-full'>
