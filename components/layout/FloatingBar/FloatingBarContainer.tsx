4374c788ed1413967354b53d1d901c6f99cda228
Manish Gupta <manishiwa@gmail.com>
2025-10-09 01:01:11 -0700

    [top bar] update shadow

--- components/layout/FloatingBar/FloatingBarContainer.tsx
+++ components/layout/FloatingBar/FloatingBarContainer.tsx
@@ -12,7 +12,7 @@ export const FloatingBarContainer: React.FC<FloatingBarContainerProps> = ({
   return (
       <div 
         data-floating-bar
-        className={`flex p-[5px] items-center w-full rounded-full bg-color-bg-medium shadow-[0px_0px_50px_0px_#000000] gap-x-[5px] md:gap-x-[15px] z-0 pointer-events-auto ${className}`}
+        className={`flex p-[5px] items-center w-full rounded-full bg-color-bg-medium shadow-card-dark gap-x-[5px] md:gap-x-[15px] z-0 pointer-events-auto ${className}`}
       >
         {children}
       </div>
