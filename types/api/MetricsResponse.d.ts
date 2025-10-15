e049cf8d9d5d76b863a2bd5d600e9d4535fab35f
Manish Gupta <manishiwa@gmail.com>
2025-10-15 02:16:00 -0700

    [fundamentals] 7d rolling fix, tone down series shadow

--- types/api/MetricsResponse.d.ts
+++ types/api/MetricsResponse.d.ts
@@ -31,6 +31,10 @@ export type ChainData = {
     types: string[];
     data: number[][];
   };
+  daily_7d_rolling: {
+    types: string[];
+    data: number[][];
+  } | undefined;
   weekly?: {
     types: string[];
     data: number[][];
