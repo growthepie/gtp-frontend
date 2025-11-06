ded9c70720329f48a62d1c47e4426ba0b5b7bd73
mokelgit <maym2018@gmail.com>
2025-11-06 13:17:50 -0500

    Fixing ath tps showing

--- components/layout/EthAgg/MetricsTop.tsx
+++ components/layout/EthAgg/MetricsTop.tsx
@@ -452,9 +452,11 @@ export const EthereumEcosystemTPSCard = React.memo(({
           <div className={`group flex flex-col gap-y-[2px] overflow-hidden ${isCompact ? 'opacity-0' : 'opacity-100'}`}>
             <div className='heading-small-xxxs text-color-ui-hover'>
               <div className='group-hover:hidden'>All-Time High</div>
-              <div className='hidden group-hover:block'>{globalMetrics.total_tps_24h_high_timestamp && globalMetrics.total_tps_ath_timestamp && globalMetrics.total_tps_24h_high_timestamp > globalMetrics.total_tps_ath_timestamp ? moment.utc(globalMetrics.total_tps_24h_high_timestamp).format("D/M/Y HH:mm UTC") : moment.utc(globalMetrics.total_tps_ath_timestamp).format("D/M/Y HH:mm UTC")}</div>
+              <div className='hidden group-hover:block'>{moment.utc(globalMetrics.total_tps_ath_timestamp).format("D/M/Y HH:mm UTC")}</div>
             </div>
-            <div className='numbers-sm'>{globalMetrics.total_tps_ath?.toLocaleString("en-GB", { maximumFractionDigits: 0 }) || 0} TPS</div>
+            {globalMetrics.total_tps_24h_high && globalMetrics.total_tps_ath && (
+              <div className='numbers-sm'>{ globalMetrics.total_tps_24h_high > globalMetrics.total_tps_ath ? globalMetrics.total_tps_24h_high?.toLocaleString("en-GB", { maximumFractionDigits: 0 }) : globalMetrics.total_tps_ath?.toLocaleString("en-GB", { maximumFractionDigits: 0 }) || 0} TPS</div>
+            )}
           </div>
           {/* 24h Peak */}
           <div className={`group flex flex-col gap-y-[2px] overflow-hidden ${isCompact ? 'opacity-0' : 'opacity-100'} transition-opacity duration-500`}>
