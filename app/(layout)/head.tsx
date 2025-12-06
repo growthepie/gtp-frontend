ce5b6ba1fbbdb52d4d039d76c4353717b51ca56e
Manish Gupta <manishiwa@gmail.com>
2025-12-05 18:07:04 -0800

    [gtm] update

--- app/(layout)/head.tsx
+++ app/(layout)/head.tsx
@@ -77,7 +77,7 @@ export default function Head() {
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              '${gtpGtmUrl}/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              '${gtpGtmUrl}/bundle.js'; f.parentNode.insertBefore(j,f);
               })(window,document,'script','dataLayer','${gtpGtmId}');`,
           }}
         />
