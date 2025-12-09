4ecc255cc366ff8f775dd23c0a98cf02ffc5ef81
Manish Gupta <manishiwa@gmail.com>
2025-12-09 13:35:49 -0800

    [analytics] route analytics through first-party endpoints for improved reliability

--- app/(debug)/layout.tsx
+++ app/(debug)/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
       {/* <Head /> */}
       <body className="sbg-white sdark:bg-color-bg-default bg-transparent text-forest-900 dark:text-color-text-primary font-raleway">
         <Providers>{children}</Providers>
-        <Analytics />
+        <Analytics scriptSrc="/api/va/script.js" />
       </body>
     </html>
   );
