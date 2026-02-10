d78994d8730cc729fb532aecb07ae4cb08fbdaa0
mokelgit <maym2018@gmail.com>
2026-02-10 13:14:17 -0500

    light mode quick bites fixes

--- app/(layout)/page.tsx
+++ app/(layout)/page.tsx
@@ -26,7 +26,7 @@ export async function generateMetadata(): Promise<Metadata> {
 export default async function Page() {
   return (
     <>
-      <Container className="flex flex-col flex-1 w-full pt-[65px] md:pt-[30px] gap-y-[10px]">
+      <Container className="flex flex-col flex-1 w-full pt-[30px] gap-y-[10px]">
         <Heading
           className="heading-large-xl max-w-[900px]"
           as="h1"
