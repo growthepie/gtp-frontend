070016fa15c2e18cacf19db3c821274e8f5cb181
Matthias Seidl <matthias@mseidl-analytics.de>
2026-02-12 12:01:52 +0100

    [menu] show treemap

--- lib/navigation.tsx
+++ lib/navigation.tsx
@@ -575,8 +575,8 @@ export const navigationItems: NavigationItem[] = [
         rootKey: "treemap",
         urlKey: "treemap",
         url: "/blockspace/treemap",
-        hide: IS_PRODUCTION,
-        excludeFromSitemap: IS_PRODUCTION,
+        //hide: IS_PRODUCTION,
+        //excludeFromSitemap: IS_PRODUCTION,
         showNew: true,
       },
       {
