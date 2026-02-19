e0488a1a56fe417f59dcab9bbc451a127aca4879
GitWave22 <135379882+GitWave22@users.noreply.github.com>
2026-02-19 13:15:15 +0000

    Contributors - updated team, text, links and growthepie logo

--- app/(layout)/contributors/layout.tsx
+++ app/(layout)/contributors/layout.tsx
@@ -40,8 +40,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
             as="h1"
           />
           <Description>
-            Meet the team and the people who make it happen. Being a public good, we rely on grants and public funding rounds, such as Gitcoin, Octant or Giveth.
-            Please support us whenever a round is active.
+            Meet the team and the people who make it happen.
           </Description>
         </Section>
       </PageContainer>
