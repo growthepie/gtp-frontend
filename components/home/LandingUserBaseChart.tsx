28011941828fcbf4f75ada6fd660266b626d5065
mokelgit <maym2018@gmail.com>
2026-02-26 10:14:25 -0500

    chart not showing

--- components/home/LandingUserBaseChart.tsx
+++ components/home/LandingUserBaseChart.tsx
@@ -246,6 +246,15 @@ export default function LandingUserBaseChart({ isLoading = false }: { isLoading?
 
 
 
+  console.log(data);
+  console.log(landing);
+  console.log(master);
+  console.log(AllChainsByKeys);
+  console.log(EnabledChainsByKeys);
+  console.log(chainAges);
+  console.log(tableChainKeys);
+  console.log(sort);
+  console.log(selectedChainTypes);
   return (
     <>
       <ShowLoading
@@ -256,15 +265,25 @@ export default function LandingUserBaseChart({ isLoading = false }: { isLoading?
 
       {data && landing && master && AllChainsByKeys ? (
         <>
-        {!IS_PRODUCTION && (
-          // <Container
-          //   className={`w-full`}
-          // >
-          //   <LandingEventsChart /> 
-          // </Container>
-          <> </>
-          )}
-        
+
+          <Container className="w-full">
+           
+          
+            <LandingChart
+              data={data}
+              master={master}
+              sources={landing.data.metrics.engagement.source}
+              cross_chain_users={data.cross_chain_users}
+              cross_chain_users_comparison={data.cross_chain_users_comparison}
+              latest_total={focusEnabled ? data.latest_total_l2 : data.latest_total}
+              latest_total_comparison={focusEnabled ? data.latest_total_comparison_l2 : data.latest_total_comparison}
+              l2_dominance={data.l2_dominance}
+              l2_dominance_comparison={data.l2_dominance_comparison}
+              selectedMetric={selectedMetric}
+              metric={selectedTimeInterval}
+              setSelectedMetric={setSelectedMetric}
+            />
+          </Container>
           <Container className="flex flex-col flex-1 w-full mt-[30px] md:mt-[60px] mb-[15px] md:mb-[15px] gap-y-[15px] justify-center">
             <div className="flex justify-between items-center">
               <div className="flex items-center gap-x-[8px] py-[10px] md:py-0">
